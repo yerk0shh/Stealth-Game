ab607f75645807bbf694923027416177182dbc13
yerk0shh <erk0shhbisenn@gmail.com>
2026-05-13 19:05:00 +0500

    feat: add player movement and collision system

---
+++ core/src/com/despat/stealth/core/Platform.java
@@ -0,0 +1,51 @@
+package com.despat.stealth.core;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.despat.stealth.utils.Collider;
+
+public class Platform
+{
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    private Vector2 position;
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    private Sprite sprite;
+
+    public Collider getCollider() {
+        return collider;
+    }
+
+    private Collider collider;
+
+    public Platform(Vector2 position, Texture texture)
+    {
+        this.position = position;
+        sprite = new Sprite(texture, texture.getWidth(), texture.getHeight());
+        collider = new Collider(position, sprite.getWidth(), sprite.getHeight());
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.begin();
+        spriteBatch.draw(sprite, position.x, position.y);
+        spriteBatch.end();
+    }
+
+    public void DrawCollider(ShapeRenderer shapeRenderer)
+    {
+        shapeRenderer.begin();
+        collider.DrawRectangle(shapeRenderer);
+        shapeRenderer.end();
+    }
+}
